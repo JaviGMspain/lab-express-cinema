117aa1fb2dd07b3b8a5101dc7d3bfe12e34396e9
Javier Godoy <javiergodoymiguez@gmail.com>
2024-05-27 12:48:34 +0200

    javi lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+    Movie.find().then((data) => {
+        res.render("movies", {movies: data});
+    });
+});
+router.get("/movies/:id", (req, res) => {
+    Movie.findById(req.params.id).then((data) =>{
+        res.render("movie", { movie: data });
+    });
+});
 
 module.exports = router;
